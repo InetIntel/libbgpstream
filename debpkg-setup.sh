9256d699dfaabe7e32550e205a8a2b99265c3464
Shane Alcock <shane@alcock.co.nz>
2025-06-05 11:53:05 +1200

    Update debpkg-setup.sh to work for debian trixie
     * software-properties-common package no longer exists
     * remove switch statement, as no longer necessary due to distros becoming EOL

--- debpkg-setup.sh
+++ debpkg-setup.sh
@@ -9,23 +9,12 @@ export DEBIAN_FRONTEND=noninteractive
 apt-get update
 apt-get install -y equivs devscripts dpkg-dev quilt curl apt-transport-https \
     apt-utils ssl-cert ca-certificates gnupg lsb-release debhelper git \
-    pkg-config sed wget sudo software-properties-common
+    pkg-config sed wget sudo
 
 DISTRO=$(lsb_release -sc)
 
 curl -1sLf 'https://dl.cloudsmith.io/public/wand/libwandio/cfg/setup/bash.deb.sh' | bash
-
-case ${DISTRO} in
-        bullseye | bookworm )
-                sed -i 's/ dh-systemd (>=1.5),//' debian/control
-        ;;
-
-        jammy )
-                sed -i 's/ dh-systemd (>=1.5),//' debian/control
-        ;;
-
-esac
-
+sed -i 's/ dh-systemd (>=1.5),//' debian/control
 
 apt-get update
 apt-get upgrade -y
